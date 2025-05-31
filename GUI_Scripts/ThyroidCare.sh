eb41c4d28406fe5064cef4691971c66eeb47a64e
niranjanjadhav4 <niranjanjadhav494@gmail.com>
2025-05-31 23:23:20 +0530

    Reorganisation of files

---
+++ GUI_Scripts/ThyroidCare.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+source /home/niranjan/Desktop/FinalProject/GUI/venv/bin/activate #use if numpy and cv2 version issues arise, any directory
+
+python3 /home/niranjan/Desktop/FinalProject/finalGUI.py  #Path to the GUI executable
